e4e9912cabff7af25f0dfdeb0a41553b57dd5f26
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-05-20 01:05:49 +0200

    prepared a theme context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useContext } from "react"
+import { ThemeContext } from "./context/theme-context-provider"
+
 export const App = () => {
-  return (
-    <div>PupFacts</div>
-  )
+  if (!ThemeContext) {
+    return (
+      <div>
+        Can't find context
+      </div>
+    )
+  } else {
+    const { theme } = useContext(ThemeContext)
+    return (
+      <>
+        <p>PupFacts</p>
+        <p>{theme}</p>
+      </>
+    )
+  }
 }
---
+++ src/context/theme-context-provider.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode, createContext, useState } from "react";
+import { ThemeType } from "../types/theme";
+
+type ThemeContextType = {
+  theme: ThemeType
+  changeTheme: (change: ThemeType) => void
+}
+
+export const ThemeContext = createContext<ThemeContextType | null>(null)
+
+type Props = {
+  children: ReactNode
+}
+
+export const ThemeContextProvider: React.FC<Props> = ({ children }) => {
+  const [theme, setTheme] = useState<ThemeType>('dark')
+
+  const changeTheme = (choice: ThemeType) => {
+    setTheme(choice)
+  }
+
+  return (
+    <ThemeContext.Provider value={{
+      theme,
+      changeTheme
+    }}>
+      <>{children}</>
+    </ThemeContext.Provider>
+  )
+}
+
