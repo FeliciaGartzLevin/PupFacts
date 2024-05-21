82b9be705e9d3c688400533536c9fafac8292bfb
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-05-21 09:41:40 +0200

    added functionality for fetching and rendering pup fact

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { useGetRandomPupFact } from './hooks/useGetRandomPupFact';
 import { useThemeContext } from './hooks/useThemeContext';
 
 export const App = () => {
-  const themeContext = useThemeContext();
+	const themeContext = useThemeContext();
+	const randomPupfact = useGetRandomPupFact();
 
-  const { theme } = themeContext;
+	const { theme } = themeContext;
 
-  return (
-    <>
-      <p>PupFacts</p>
-      <p>{theme}</p>
-    </>
-  );
+	return (
+		<>
+			<p>PupFacts</p>
+			<p>{theme}</p>
+
+			{randomPupfact ? <p>{randomPupfact}</p> : null}
+		</>
+	);
 };
