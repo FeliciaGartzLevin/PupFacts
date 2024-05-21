ff97f4dcfaf21c0985a2384550aaecca1646d115
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-05-21 22:26:23 +0200

    added a custom hook for using local storage (copy from johan the teacher just for me to fathom it - which I do now 😁), and used it in the context so that the chosen theme is saved in localstorage yaay

--- src/context/theme-context-provider.tsx
+++ src/context/theme-context-provider.tsx
@@ -1,32 +1,34 @@
 import React, { ReactNode, createContext, useState } from 'react';
 import { ThemeType } from '../types/theme';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 type ThemeContextType = {
-  theme: ThemeType;
-  changeTheme: (change: ThemeType) => void;
+	theme: ThemeType;
+	changeTheme: (change: ThemeType) => void;
 };
 
 export const ThemeContext = createContext<ThemeContextType | null>(null);
 
 type Props = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 export const ThemeContextProvider: React.FC<Props> = ({ children }) => {
-  const [theme, setTheme] = useState<ThemeType>('dark');
+	const [theme, setTheme] = useLocalStorage<ThemeType>('theme', 'dark');
+	// const [storedTheme, setStoredTheme] =
 
-  const changeTheme = (choice: ThemeType) => {
-    setTheme(choice);
-  };
+	const changeTheme = (choice: ThemeType) => {
+		setTheme(choice);
+	};
 
-  return (
-    <ThemeContext.Provider
-      value={{
-        theme,
-        changeTheme,
-      }}
-    >
-      <>{children}</>
-    </ThemeContext.Provider>
-  );
+	return (
+		<ThemeContext.Provider
+			value={{
+				theme,
+				changeTheme,
+			}}
+		>
+			<>{children}</>
+		</ThemeContext.Provider>
+	);
 };
