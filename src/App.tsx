107c7e0ece2bcb773167911421e1053cd60c662a
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-05-22 01:31:47 +0200

    configured tailwind to prepare for styling later

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import { Home } from './pages/home';
 import { Footer } from './pages/partials/footer';
 import { Navigation } from './pages/partials/navigation';
 import { Routes, Route } from 'react-router-dom';
+import './index.css';
+
+//install clsx and conditionally render font and bg colors based on current theme
 
 export const App = () => (
-	<div id="App" className="">
-		<Navigation className={'h-20'} />
+	<div id="App" className="bg-gray-800 text-white">
+		<div className="h-[10%] w-full full-app-side-spacing">
+			<Navigation />
+		</div>
 
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/about" element={<About />} />
-		</Routes>
+		<div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</div>
 
-		<Footer className={'h-20'} />
+		<div className="h-[20%] w-full full-app-side-spacing">
+			<Footer className={'h-20 py-full-app-py'} />
+		</div>
 	</div>
 );
