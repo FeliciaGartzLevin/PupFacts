3dbc58d82f80a30c3f04673324159aed4dd52d7a
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2024-05-22 14:47:13 +0200

    added a npm package 'tailwind-theme-swapper' for swapping theme width tailwind classes, and the functionality for making it work with the theme value in the context.

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,31 @@ import { Footer } from './pages/partials/footer';
 import { Navigation } from './pages/partials/navigation';
 import { Routes, Route } from 'react-router-dom';
 import './index.css';
+import { useThemeColors } from './hooks/use-theme-colors';
 
 //install clsx and conditionally render font and bg colors based on current theme
 
-export const App = () => (
-	<div id="App" className="bg-gray-800 text-white">
-		<div className="h-[10%] w-full full-app-side-spacing">
-			<Navigation />
-		</div>
+export const App = () => {
+	const cssClasses = useThemeColors();
 
-		<div>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</div>
+	return (
+		<div id="App" className={cssClasses}>
+			<div className="bg-primary text- text-primary bg-backgroundColor-primary">
+				<div className="h-[10%] w-full full-app-side-spacing">
+					<Navigation />
+				</div>
 
-		<div className="h-[20%] w-full full-app-side-spacing">
-			<Footer className={'h-20 py-full-app-py'} />
+				<div>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+					</Routes>
+				</div>
+
+				<div className="h-[20%] w-full full-app-side-spacing">
+					<Footer className={'h-20 py-full-app-py'} />
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,54 @@
 /** @type {import('tailwindcss').Config} */
+import themeSwapper from 'tailwindcss-theme-swapper';
+
+/* Themes */
+const darkTheme = {
+	colors: {
+		accentColor: {
+			dark: '#D90368',
+			light: '#FFD400',
+		},
+		backgroundColor: {
+			primary: '#541388', //#2E294E
+		},
+		textColor: {
+			primary: '#F1E9DA',
+		},
+	},
+};
+
+const lightTheme = {
+	colors: {
+		accentColor: {
+			dark: '#007090',
+			light: '#FF88DC',
+		},
+		backgroundColor: {
+			primary: '#F1E9DA',
+		},
+		textColor: {
+			primary: '#2E294E',
+		},
+	},
+};
+
+const pinkTheme = {
+	colors: {
+		accentColor: {
+			dark: '#7E5A9B',
+			light: '#9CF6F6',
+		},
+		backgroundColor: {
+			primary: '#EDADCA',
+		},
+		textColor: {
+			primary: '#63458A',
+		},
+	},
+};
+
+/* Tailwind config */
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
@@ -8,5 +58,25 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		themeSwapper({
+			themes: [
+				{
+					name: 'base',
+					selectors: [':root'],
+					theme: darkTheme,
+				},
+				{
+					name: 'light',
+					selectors: ['.light'],
+					theme: lightTheme,
+				},
+				{
+					name: 'pink',
+					selectors: ['.pink'],
+					theme: pinkTheme,
+				},
+			],
+		}),
+	],
 };
